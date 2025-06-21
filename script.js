23fa9645078c1c04d3b15649d70428872484a5ff
VIVEK KUMAR <vk0730267@gmail.com>
2025-06-21 21:21:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  document.body.innerText = "DOM load success";
+});
